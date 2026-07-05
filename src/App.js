407357eda2a102f9ccf10afb8d7a6d097a0861ff
mohammedhhussain786 <mohammedh.hussain786@gmail.com>
2026-07-05 11:51:42 +0530

    Experiment B - Functional Counter

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React, { useState } from "react";
 
 function App() {
 
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div>
+      <h1>Functional Counter</h1>
 
-      <h1>Conditional Rendering</h1>
+      <h2>{count}</h2>
 
-      {
-        loggedIn
-          ? <h2>Welcome Mohammed</h2>
-          : <h2>Please Login</h2>
-      }
-
-      <button
-        onClick={() => setLoggedIn(!loggedIn)}
-      >
-        Toggle
+      <button onClick={increment}>
+        Increment
       </button>
-
     </div>
   );
 }
